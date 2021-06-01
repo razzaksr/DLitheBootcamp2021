f22dc6c9551d1a3fce7bac216e2160884ce44124
razzaksr <razzaksr@gmail.com>
2021-06-01 18:59:02 +0530

    Simply new console

---
+++ DLitheConsole/src/main/java/application/console/dlithe/bootcamp/DLitheConsole/App.java
@@ -0,0 +1,13 @@
+package application.console.dlithe.bootcamp.DLitheConsole;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
